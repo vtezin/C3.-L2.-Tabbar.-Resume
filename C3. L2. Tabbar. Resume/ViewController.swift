7ed97db29fee1d20f7c99076f3c65e4c6dd19ea3
Tezin Vladimir <vtezin@gmail.com>
2019-04-26 07:35:09 +0300

    Initial Commit

---
+++ C3. L2. Tabbar. Resume/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  C3. L2. Tabbar. Resume
+//
+//  Created by Admin on 26/04/2019.
+//  Copyright © 2019 Vladimir Tezin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
